a8f8df6941d15a19e00f9d51cb9a814fa606fd02
Luckie Bah <bah966@gmail.com>
2023-03-21 13:16:02 +0000

    finished the addtocollection function made the objects with titl,arti,yrpub and returned it

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,4 +1,16 @@
 console.log('***** Music Collection *****')
 
 // created a variable called 'collection'
-let collection=[];
+let collection = [];
+let music = {
+    title: '',
+    artist: '',
+    yearPublished: ''
+}
+collection.push(music);
+
+function addToCollection(title, artist, yearPublished) {
+    return music;
+}
+console.log('should say title,artist,year published ', addToCollection())
+
