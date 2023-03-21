b05c1c59b9468ae5d6a42d803e90ad1b4db71de0
Luckie Bah <bah966@gmail.com>
2023-03-21 13:52:25 +0000

    added list of artist title and yrpub push object with properties to collection var

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,14 +3,23 @@ console.log('***** Music Collection *****')
 // created a variable called 'collection'
 let collection = [];
 let music = {
-    title: '',
-    artist: '',
-    yearPublished: ''
+    title: [],
+    artist: [],
+    yearPublished: []
 }
-collection.push(music);
 
+collection.push(music)
 function addToCollection(title, artist, yearPublished) {
-    return music;
+   music.title.push(title)
+   music.artist.push(artist)
+   music.yearPublished.push(yearPublished)
+   
+   return music;
 }
-console.log('should say title,artist,year published ', addToCollection())
-
+console.log('should add param', addToCollection('Reasonable doubt','Jay z','1996'))
+console.log('should add param', addToCollection('444','Jay z','2017'))
+console.log('should add param', addToCollection('Igor','Tyler','2019'))
+console.log('should add param', addToCollection('Illmatic','Nas','1994'))
+console.log('should add param', addToCollection('Lioness','Amy whinehouse','2011'))
+console.log('should add param', addToCollection('Carnival','Briston Maroney','2018'))
+console.log(collection)
